95ff5c8d73e57441b60ad26910d352fbaf297617
xdou <1161906502@qq.com>
2021-09-27 08:56:12 +0800

    完成了订单页面的功能开发

--- src/main.js
+++ src/main.js
@@ -15,8 +15,10 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+// axios.defaults.headers.get['Content-Type'] = 'application/json;charset=UTF-8'
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
 
 axios.interceptors.request.use((config) => {
   // console.log(config)
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -28,7 +28,9 @@ import {
   Step,
   CheckboxGroup,
   Checkbox,
-  Upload
+  Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -66,6 +68,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 // Message消息提示框需全局挂载
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
